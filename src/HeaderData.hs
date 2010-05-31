c121b427a1874538c6f0ab626642815078f250ca
Antti Salonen <ajsalonen@gmail.com>
2010-05-31 12:25:29 +0200

    split Main to Main, HeaderData and HeaderParser modules

---
+++ src/HeaderData.hs
@@ -0,0 +1,49 @@
+module HeaderData
+where
+
+type Type = String
+
+data ParamDecl = ParamDecl {
+    varname   :: String
+  , vartype   :: Type
+  , varvalue  :: Maybe String
+  , vararray  :: Maybe String
+  }
+  deriving (Eq, Read, Show)
+
+data Object = FunDecl {
+                funname      :: String
+              , rettype      :: Type
+              , params       :: [ParamDecl]
+              , fnnamespace  :: [String]
+              , fnvisibility :: Maybe (InheritLevel, String)
+              }
+            | Namespace String [Object]
+            | TypeDef (String, String)
+            | ClassDecl {
+                classname     :: String
+              , classinherits :: [InheritDecl]
+              , classobjects  :: [Object]
+              }
+            | VarDecl ParamDecl (Maybe (InheritLevel, String))
+            | EnumDef String [EnumVal]
+    deriving (Eq, Read, Show)
+
+data InheritDecl = InheritDecl {
+    inheritname  :: String
+  , inheritlevel :: InheritLevel
+  }
+  deriving (Eq, Read, Show)
+
+data InheritLevel = Public | Protected | Private
+  deriving (Eq, Read, Show, Enum, Bounded)
+
+data EnumVal = EnumVal {
+    enumvaluename :: String
+  , enumvalue     :: Maybe Int
+  }
+  deriving (Eq, Read, Show)
+
+type Header = [Object]
+
+
